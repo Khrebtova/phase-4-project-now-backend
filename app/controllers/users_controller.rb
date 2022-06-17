86db7926f77dd0585a89b552bf7db43a8b27166f
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-06-17 13:53:29 -0700

    addded signup, login, logout routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,48 +1,54 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show update destroy ]
+  # before_action :set_user, only: %i[ show update destroy ]
+  
+  # GET /me
+  def show
+    @user = User.find_ny(id: sessions[:user_id])
+    if @user
+      render json: @user, status: :ok
+    else
+      render json: { errors: ['Unauthorized'] }, status: :Unauthorized
+    end
+  end
+  
+  # POST /signup
+  def create
+    @user = User.create(user_params)
+    
+    if @user.valid?
+      render json: @user, status: :created
+      sessions[:user_id] = @user.id
+    else
+      render json: @user.errors.full_massages, status: :unprocessable_entity
+    end
+  end
+
 
   # GET /users
-  def index
-    @users = User.all
-
-    render json: @users
-  end
-
-  # GET /users/1
-  def show
-    render json: @user
-  end
-
-  # POST /users
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      render json: @user, status: :created, location: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
+  # def index
+  #   @users = User.all
+  #   render json: @users
+  # end
 
   # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   if @user.update(user_params)
+  #     render json: @user
+  #   else
+  #     render json: @user.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # DELETE /users/1
-  def destroy
-    @user.destroy
-  end
+  # def destroy
+  #   @user.destroy
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+    # def set_user
+    #   @user = User.find_by(id: params[:id])
+    # end
 
     # Only allow a list of trusted parameters through.
     def user_params
